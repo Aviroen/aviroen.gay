31367ab17c4f10d6ef5d9782420556d8bb50ae18
Aviroen <aviroen@gmail.com>
2026-06-10 12:17:20 -0700

    i think i'm funny

--- js/comic_footer.js
+++ js/comic_footer.js
@@ -2,6 +2,6 @@
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
-
+    <a href="index.html"><img src="/img/header_scoop.png" alt="" /></a>
     </footer>
 `;
