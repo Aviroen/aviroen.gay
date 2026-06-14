c9e7a44d7cb292cc4e0af12c03a4ad8777a42576
Aviroen <aviroen@gmail.com>
2026-06-14 08:48:07 -0700

    tsss

--- js/comic_header.js
+++ js/comic_header.js
@@ -5,15 +5,15 @@ document.querySelector(".writeHeader").innerHTML = `
         <a href="index.html"><img src="/img/header.png" alt="" /></a> 
 
         <div id="nav">
-            <a href="index.html"><button class="button button3">HOME</button></a>
-            <a href="https://ggmods.com/profile/QA733654/"><button class="button button3">GGMODS</button></a>
-            <a href="https://www.nexusmods.com/profile/aviroen"><button class="button button3">NEXUSMODS</button></a>
-            <a href="https://archiveofourown.org/users/Aviroen/pseuds/Aviroen.html"><button class="button button3">AO3</button></a>
-            <a href="https://ko-fi.com/aviroen"><button class="button button3">KO-FI</button></a>
-            <a href="https://bsky.app/profile/aviroen.bsky.social"><button class="button button3">BSKY</button></a>
-            <a href="comic.html"><button class="button button3">COMIC</button></a>
-            <a href="sdvmods.html"><button class="button button3">SDV MODS</button></a>
-            <a href="friends.html"><button class="button button3">FRIENDS</button></a>
+            <a href="index.html"><button class="button">HOME</button></a>
+            <a href="https://ggmods.com/profile/QA733654/"><button class="button">GGMODS</button></a>
+            <a href="https://www.nexusmods.com/profile/aviroen"><button class="button">NEXUSMODS</button></a>
+            <a href="https://archiveofourown.org/users/Aviroen/pseuds/Aviroen.html"><button class="button">AO3</button></a>
+            <a href="https://ko-fi.com/aviroen"><button class="button">KO-FI</button></a>
+            <a href="https://bsky.app/profile/aviroen.bsky.social"><button class="button">BSKY</button></a>
+            <a href="comic.html"><button class="button">COMIC</button></a>
+            <a href="sdvmods.html"><button class="button">SDV MODS</button></a>
+            <a href="friends.html"><button class="button">FRIENDS</button></a>
         </div>
     </header>
 `;
